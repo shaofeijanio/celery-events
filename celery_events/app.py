6fb17769998e18b86cad026b08e2ca6bf65cc7fe
fei <shaofei330@gmail.com>
2019-07-26 15:59:06 +0800

    Do not sync when no backend_class is provided in App

--- celery_events/app.py
+++ celery_events/app.py
@@ -4,8 +4,8 @@ from celery_events.events import Registry
 
 class App:
 
-    def __init__(self, backend_class, get_broadcast_queue=None, get_task_name_queue=None):
-        if not issubclass(backend_class, Backend):
+    def __init__(self, backend_class=None, get_broadcast_queue=None, get_task_name_queue=None):
+        if self.backend_class is not None and not issubclass(backend_class, Backend):
             raise TypeError('backend_class is not a subclass of Backend.')
 
         self.backend_class = backend_class
@@ -20,16 +20,19 @@ class App:
         return self.backend_class(self.registry)
 
     def update_local_events(self):
-        backend = self._get_backend()
-        backend.update_local_events()
+        if self.backend_class:
+            backend = self._get_backend()
+            backend.update_local_events()
 
     def sync_local_events(self):
-        backend = self._get_backend()
-        backend.sync_local_events()
+        if self.backend_class:
+            backend = self._get_backend()
+            backend.sync_local_events()
 
     def sync_remote_events(self):
-        backend = self._get_backend()
-        backend.sync_remote_events()
+        if self.backend_class:
+            backend = self._get_backend()
+            backend.sync_remote_events()
 
 
 class AppContainer:
