63b444b61adf6ff005da48ed159d98dc32e3373b
fei <shaofei330@gmail.com>
2019-07-26 16:08:48 +0800

    Fix App

--- celery_events/__init__.py
+++ celery_events/__init__.py
@@ -3,11 +3,11 @@ from celery_events.app import App, AppContainer
 app_container = AppContainer()
 
 
-def create_app(backend_class, get_broadcast_queue=None, get_task_name_queue=None):
+def create_app(backend_class=None, get_broadcast_queue=None, get_task_name_queue=None):
     """Convenience method to create app."""
 
     app_container.app = App(
-        backend_class,
+        backend_class=backend_class,
         get_broadcast_queue=get_broadcast_queue,
         get_task_name_queue=get_task_name_queue
     )
