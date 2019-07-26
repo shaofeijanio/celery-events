be5e1c7af0dd3b61c8b71b5d4000e2ce04c6a6ef
fei <shaofei330@gmail.com>
2019-07-26 16:06:49 +0800

    Fix App

--- celery_events/app.py
+++ celery_events/app.py
@@ -5,15 +5,15 @@ from celery_events.events import Registry
 class App:
 
     def __init__(self, backend_class=None, get_broadcast_queue=None, get_task_name_queue=None):
-        if self.backend_class is not None and not issubclass(backend_class, Backend):
+        if backend_class and not issubclass(backend_class, Backend):
             raise TypeError('backend_class is not a subclass of Backend.')
 
         self.backend_class = backend_class
         self.registry = Registry()
 
-        if get_broadcast_queue is not None:
+        if get_broadcast_queue:
             self.registry.set_get_broadcast_queue(get_broadcast_queue)
-        if get_task_name_queue is not None:
+        if get_task_name_queue:
             self.registry.set_get_task_name_queue(get_task_name_queue)
 
     def _get_backend(self):
